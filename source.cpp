4a5ea032c753fdf0dd9110651ec29a936cd123cd
vophison <phison.vps.19@gmail.com>
2020-10-21 10:29:51 +0700

    son update

--- source.cpp
+++ source.cpp
@@ -1,15 +1,14 @@
+#include "iostream"
+using namespace std;
 int USCLN(int a, int b)
 {
 	cout << "Uoc chung lon nhat"
 }
-void hello()
-{
-	
-	cout<<"Chao mung ban";
-}
+
 int tru(int a, int b)
 {
-	cout<<a-b;
+	cout << a - b;
+}
 int cong(int a, int b)
 {
 	cout << a + b;
